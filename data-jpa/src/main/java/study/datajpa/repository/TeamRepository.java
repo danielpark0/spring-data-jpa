cddc28b2088152410b9b9c37b8b78811fdefb18b
danielpark0 <37523983+danielpark0@users.noreply.github.com>
2022-01-14 23:11:11 +0900

    namedQuery

--- data-jpa/src/main/java/study/datajpa/repository/TeamRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/TeamRepository.java
@@ -3,5 +3,6 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Team;
 
-public interface TeamRepository extends JpaRepository<Team, Long> {
+public interface
+TeamRepository extends JpaRepository<Team, Long> {
 }
